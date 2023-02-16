2d53c9c545ddaea3eefa38082236dad5811b1af2
codinglcy <codinglcy@gmail.com>
2023-02-16 21:42:41 +0900

    🎨 axios 주석 해제

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  // const [hello, setHello] = useState("");
+  const [hello, setHello] = useState("");
 
-  // useEffect(() => {
-  //   axios
-  //     .get("/api/hello")
-  //     .then((res) => setHello(res.data))
-  //     .catch((err) => console.log(err));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then((res) => setHello(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src="./logo512.png" className="App-logo" alt="logo" />
-        <p>hello</p>
+        <div>{hello}</div>
         <a
           className="App-link"
           href="https://reactjs.org"
