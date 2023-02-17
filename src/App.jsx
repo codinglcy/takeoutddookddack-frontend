622e3e817760b218ac664d2499cc8ea1cb75aef4
codinglcy <codinglcy@gmail.com>
2023-02-18 02:33:33 +0900

    🔥🎨 불필요한 코드, 파일 삭제 / js파일 jsx로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,6 @@ function App() {
       <header className="App-header">
         <img src="./logo512.png" className="App-logo" alt="logo" />
         <div>{hello}</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
