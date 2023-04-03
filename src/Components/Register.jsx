2a595b70f42267d31344e1c39632f055da4de67c
codinglcy <codinglcy@gmail.com>
2023-04-03 20:19:19 +0900

    ✨ 회원가입 폼 input 칸

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,42 @@
 import "./css/Register.css";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
 const Register = () => {
   return (
     <>
       <div>판매뚝딱 회원가입폼</div>
+      <InputGroup className="mb-3">
+        {/* <Form.Control
+          placeholder="id"
+          aria-label="id"
+          aria-describedby="basic-addon2"
+        /> */}
+        <FloatingLabel controlId="id" label="Id">
+          <Form.Control type="text" placeholder="id" />
+        </FloatingLabel>
+        <Button variant="outline-secondary" id="button-addon2">
+          Button
+        </Button>
+      </InputGroup>
+      <FloatingLabel controlId="password" label="Password" className="mb-3">
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+      <FloatingLabel
+        controlId="checkPW"
+        label="Check Password"
+        className="mb-3"
+      >
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+      <FloatingLabel controlId="email" label="Email address" className="mb-3">
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+      <FloatingLabel controlId="name" label="Name" className="mb-3">
+        <Form.Control type="text" placeholder="name" />
+      </FloatingLabel>
     </>
   );
 };
