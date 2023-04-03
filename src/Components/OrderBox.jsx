144a25ee0e45a78262a7b6b5f5a12119cd4ac7f3
codinglcy <codinglcy@gmail.com>
2023-04-03 19:47:29 +0900

    ✨ 모달로 정보 입력받은 후 출력 기능

--- src/Components/OrderBox.jsx
+++ src/Components/OrderBox.jsx
@@ -1,9 +1,48 @@
 import "./css/OrderBox.css";
+import React, { useState } from "react";
+import OrderForm from "./OrderForm";
 
 const OrderBox = () => {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+
+  const [orderInfo, setOrderInfo] = useState({
+    info1: "11111",
+    info2: "22222",
+    info3: "33333",
+    telNum: "",
+    name: "",
+  });
+
+  const parentFunc = (orderInfor) => {
+    setOrderInfo((current) => {
+      let newInfo = { ...current };
+      newInfo["telNum"] = orderInfor["telNum"];
+      newInfo["name"] = orderInfor["name"];
+      return newInfo;
+    });
+  };
+
   return (
     <>
       <div>주문뚝딱 주문서&주문자정보입력버튼</div>
+      <button onClick={handleShow}>주문자 정보 입력</button>
+      <OrderForm
+        show={show}
+        setShow={setShow}
+        orderInfo={orderInfo}
+        setOrderInfo={setOrderInfo}
+        parentFunc={parentFunc}
+      />
+
+      <button
+        onClick={() => {
+          console.log(JSON.stringify(orderInfo));
+        }}
+      >
+        정보보기
+      </button>
     </>
   );
 };
--- src/Components/OrderForm.jsx
+++ src/Components/OrderForm.jsx
@@ -1,9 +1,71 @@
 import "./css/OrderForm.css";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
-const OrderForm = () => {
+const OrderForm = (props) => {
+  const [show, setShow] = [props.show, props.setShow];
+
+  const handleClose = () => setShow(false);
+
+  const orderInfo = props.orderInfo;
+
   return (
     <>
       <div>주문뚝딱 주문자정보입력폼</div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FloatingLabel
+            controlId="telNum"
+            label="주문자 전화번호"
+            className="mb-3"
+          >
+            <Form.Control
+              type="tel"
+              placeholder="tel"
+              defaultValue={
+                orderInfo["telNum"] ? orderInfo["telNum"] : undefined || ""
+              }
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="name" label="주문자명">
+            <Form.Control
+              type="text"
+              placeholder="text"
+              defaultValue={
+                orderInfo["name"] ? orderInfo["name"] : undefined || ""
+              }
+            />
+          </FloatingLabel>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            취소
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleClose();
+              orderInfo["telNum"] = document.getElementById("telNum").value;
+              orderInfo["name"] = document.getElementById("name").value;
+              console.log(JSON.stringify(orderInfo));
+              props.parentFunc(orderInfo);
+            }}
+          >
+            완료
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
