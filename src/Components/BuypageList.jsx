2679e595075377c0cadcc6e263733eb08ad7b930
codinglcy <codinglcy@gmail.com>
2023-04-07 00:56:41 +0900

    🎨 가게 목록 박스 (첫 화면 전체 가게 표시)

--- src/Components/BuypageList.jsx
+++ src/Components/BuypageList.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react";
 import "./css/BuypageList.css";
 import SearchByAddress from "./SearchByAddress";
+import axios from "axios";
 
 const BuypageList = () => {
+  const [shopList, setShopList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/shop/all").then((res) => {
+      setShopList(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <>
       <div>가게 목록</div>
       <SearchByAddress />
+      <div className="listBox">
+        <table className="table">
+          <colgroup>
+            <col width="150px" />
+            <col width="80px" />
+            <col width="100px" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>가게 위치</th>
+              <th>가게 상태</th>
+              <th>가게 url</th>
+            </tr>
+          </thead>
+          <tbody className="listBody">
+            {shopList &&
+              shopList.map((shop) => {
+                return (
+                  <tr key={shop.id}>
+                    <td>{shop.location}</td>
+                    <td>{shop.open ? "영업중" : "준비중"}</td>
+                    <td>{shop.shopUrl}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
