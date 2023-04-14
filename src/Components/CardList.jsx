9e5b04635f1ad3765ec7740367349b5d989ad4cd
codinglcy <codinglcy@gmail.com>
2023-04-15 04:38:22 +0900

    ✨🎨 컴포넌트 완료 (css제외)
    - 데이터를 받아 정렬 후 카드로 하나씩 전달
    - 카드들의 길이가 div를 초과할 시 스크롤, 스크롤은 보이지 않도록 숨김

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -1,10 +1,53 @@
+import { useEffect, useState } from "react";
+import OrderCard from "./OrderCard";
 import "./css/CardList.css";
+import axiosApi from "../Util/api";
+import getAccessToken from "../Util/checkAccessToken";
 
 const CardList = () => {
+  const [orders, setOrders] = useState([]);
+
+  const getOrdersFunc = () => {
+    axiosApi
+      .get("/api/order", {
+        headers: {
+          Authorization: `Bearer ${getAccessToken()}`,
+        },
+      })
+      .then((res) => {
+        const orderBy = ["Check", "New", "Ready"];
+        let data = res.data;
+        data.sort(
+          (a, b) =>
+            orderBy.indexOf(a.status) - orderBy.indexOf(b.status) ||
+            a.createdAt - b.createdAt
+        );
+        setOrders(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getOrdersFunc();
+  }, []);
+
   return (
-    <>
+    <div className="cardList">
       <div>판매뚝딱 주문카드 리스트</div>
-    </>
+      {orders &&
+        orders.map((orderInfo, idx) => {
+          return (
+            <OrderCard
+              key={idx}
+              idx={idx}
+              orderInfo={orderInfo}
+              getOrdersFunc={getOrdersFunc}
+            />
+          );
+        })}
+    </div>
   );
 };
 
