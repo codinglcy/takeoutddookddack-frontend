46300ad189748a31e8bc9e0cfef1313e51063270
codinglcy <codinglcy@gmail.com>
2023-04-15 04:36:37 +0900

    ✨🎨 컴포넌트 완료 (css제외)
    - CardList컴포넌트에서 카드에 들어갈 정보를 props로 받아 카드 생성
    - 카드 삭제버튼: 해당 카드에 해당하는 주문 건 삭제 (삭제 전 확인창)
    - 각 상태를 나타내는 버튼 선택시 axios로 해당 주문건의 db 수정
    - 어떤 상태에 있을 때 해당 상태의 버튼 비활성화

--- src/Components/OrderCard.jsx
+++ src/Components/OrderCard.jsx
@@ -1,9 +1,133 @@
+import { useEffect, useState } from "react";
 import "./css/OrderCard.css";
+import { Card } from "react-bootstrap";
+import axiosApi from "../Util/api";
 
-const OrderCard = () => {
+const OrderCard = (props) => {
+  const [order, setOrder] = useState({});
+  const [idx, setIdx] = useState();
+
+  const removeOrder = (id, idx) => {
+    if (window.confirm(`${idx}번 카드의 주문 건을 삭제하시겠습니까?`)) {
+      axiosApi.delete(`/api/order/${id}`);
+      props.getOrdersFunc();
+    }
+  };
+
+  const editStatus = (id, status) => {
+    axiosApi.patch(`/api/order/${id}?status=${status}`);
+    props.getOrdersFunc();
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIdx(props.idx);
+      setOrder(props.orderInfo);
+      console.log(props.idx, props.orderInfo);
+    }, 100);
+  }, [props.idx, props.orderInfo]);
+
   return (
     <>
-      <div>판매뚝딱 주문카드</div>
+      <div id="number">{idx + 1}</div>
+      <button id="closeBtn" onClick={() => removeOrder(order.id, idx + 1)}>
+        x
+      </button>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <div>
+            입금자명:
+            <input
+              style={{
+                border: "none",
+                paddingLeft: "40px",
+                width: "187px",
+                textAlign: "center",
+              }}
+              readOnly
+              value={order.orderName || ""}
+            />
+          </div>
+
+          <div>
+            전화번호:
+            <input
+              style={{
+                border: "none",
+                paddingLeft: "40px",
+                width: "187px",
+                textAlign: "center",
+              }}
+              readOnly
+              value={order.orderTel || ""}
+            />
+          </div>
+
+          <div>
+            주문내용
+            <input
+              style={{
+                border: "none",
+                paddingLeft: "40px",
+                width: "187px",
+                textAlign: "right",
+              }}
+              readOnly
+              value={`총 합계:  ${order.totalPrice}원` || "총 합계: 0원"}
+            />
+            <div style={{ border: "solid" }}>
+              <table className="table">
+                <colgroup>
+                  <col width="60%" />
+                  <col width="40%" />
+                </colgroup>
+                <tbody>
+                  {order.selectMenu &&
+                    order.selectMenu.map((menu) => {
+                      return (
+                        <tr key={menu.item}>
+                          <td>{menu.item}</td>
+                          <td>X{menu.quantity}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          {Object.is(order.status, "New") ? (
+            <button disabled={true} onClick={() => editStatus(order.id, "New")}>
+              새주문
+            </button>
+          ) : (
+            <button onClick={() => editStatus(order.id, "New")}>새주문</button>
+          )}
+          {Object.is(order.status, "Check") ? (
+            <button
+              disabled={true}
+              onClick={() => editStatus(order.id, "Check")}
+            >
+              입금확인
+            </button>
+          ) : (
+            <button onClick={() => editStatus(order.id, "Check")}>
+              입금확인
+            </button>
+          )}
+          {Object.is(order.status, "Ready") ? (
+            <button
+              disabled={true}
+              onClick={() => editStatus(order.id, "Ready")}
+            >
+              준비완료
+            </button>
+          ) : (
+            <button onClick={() => editStatus(order.id, "Ready")}>
+              준비완료
+            </button>
+          )}
+        </Card.Body>
+      </Card>
     </>
   );
 };
