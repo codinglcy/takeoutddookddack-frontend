abb4e7c9d92dfbc79f73a5b8b1ba18daa110260e
codinglcy <codinglcy@gmail.com>
2023-04-16 03:48:00 +0900

    ✨ 각 상태의 버튼 생성

--- src/Components/StatusType.jsx
+++ src/Components/StatusType.jsx
@@ -4,7 +4,10 @@ const StatusType = () => {
   return (
     <>
       <div>주문뚝딱 주문건 상태 종류</div>
-      <div>판매뚝딱 주문건 상태 종류</div>
+      <button>전체 보기</button>
+      <button>새주문</button>
+      <button>입금확인</button>
+      <button>준비완료</button>
     </>
   );
 };
