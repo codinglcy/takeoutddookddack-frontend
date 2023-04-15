e6b94758b91c6355d94d45d50f3aa27cab307fca
codinglcy <codinglcy@gmail.com>
2023-04-16 03:49:10 +0900

    ✨🎨 가게에 들어온 주문 내용을 표시

--- src/Components/SellingSituation.jsx
+++ src/Components/SellingSituation.jsx
@@ -1,9 +1,94 @@
+import { useEffect, useState } from "react";
 import "./css/SellingSituation.css";
+import axiosApi from "../Util/api";
 
-const SellingSituation = () => {
+const SellingSituation = (props) => {
+  const sellerId = props.sellerId;
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = () => {
+    axiosApi
+      .get(`/api/order/shop?sellerId=${sellerId}`)
+      .then((res) => {
+        const orderBy = ["Check", "New", "Ready"];
+        let data = res.data;
+        data.sort(
+          (a, b) =>
+            orderBy.indexOf(a.status) - orderBy.indexOf(b.status) ||
+            a.createdAt - b.createdAt
+        );
+        setOrders(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const statusShow = (status) => {
+    if (status === "New") {
+      return <div>새주문</div>;
+    } else if (status === "Check") {
+      return <div>입금확인</div>;
+    } else {
+      return <div>준비완료</div>;
+    }
+  };
+
+  useEffect(() => {
+    getOrders();
+  });
+
   return (
     <>
       <div>주문뚝딱 가게판매현황</div>
+      <div className="situation">
+        <div>영업 현황</div>
+        <table className="situationTable">
+          <colgroup>
+            <col width="25%" />
+            <col width="*" />
+            <col width="25%" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>{"주문자 전화번호\n뒷자리"}</th>
+              <th>주문 내용</th>
+              <th>진행 상태</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders &&
+              orders.map((order, idx) => {
+                return (
+                  <tr key={idx}>
+                    <td>
+                      {
+                        order.orderTel.split("-")[
+                          order.orderTel.split("-").length - 1
+                        ]
+                      }
+                    </td>
+                    <td>
+                      {order.selectMenu.length === 1
+                        ? order.selectMenu[0].item
+                        : order.selectMenu.length === 2
+                        ? order.selectMenu[0].item +
+                          ", " +
+                          order.selectMenu[1].item
+                        : order.selectMenu[0].item +
+                          ", " +
+                          order.selectMenu[1].item +
+                          " 외 " +
+                          (order.selectMenu.length - 2) +
+                          "종"}
+                    </td>
+                    <td>{statusShow(order.status)}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
--- src/Pages/BuyPage.jsx
+++ src/Pages/BuyPage.jsx
@@ -2,21 +2,33 @@ import StatusType from "../Components/StatusType";
 import ShopInfo from "../Components/ShopInfo";
 import SellingSituation from "../Components/SellingSituation";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axiosApi from "../Util/api";
 
 const BuyPage = (props) => {
-  const params = useParams();
-  console.log(params.sellerId);
+  const sellerId = useParams().sellerId;
+  const [shopInfo, setShopInfo] = useState({});
 
   useEffect(() => {
     props.isBuyPageFunc(true);
-  }, [props]);
+    axiosApi
+      .get(`/api/shop/order?sellerId=${sellerId}`)
+      .then((res) => {
+        setShopInfo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [props, sellerId]);
 
   return (
     <div className="Pages">
       <div>주문뚝딱 가게 페이지입니다.</div>
-      <ShopInfo />
-      <SellingSituation />
+      <ShopInfo
+        location={shopInfo.location}
+        bankAccount={shopInfo.bankAccount}
+      />
+      <SellingSituation sellerId={sellerId} />
       <StatusType />
     </div>
   );
