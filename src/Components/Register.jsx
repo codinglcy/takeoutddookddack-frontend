742a28bad97f6e1a620a87ee953000204bb08227
codinglcy <codinglcy@gmail.com>
2023-04-18 20:24:21 +0900

    🎨 회원수정일 경우 회원 정보 default값 설정 & 라벨에 한글 추가

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -3,35 +3,77 @@ import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
+import axiosApi from "../Util/api";
+import getAccessToken from "../Util/checkAccessToken";
 
-const Register = () => {
+const Register = (props) => {
+  const [seller, setSeller] = useState({});
+
+  useEffect(() => {
+    if (props.newedit === "edit") {
+      axiosApi
+        .get("/api/seller", {
+          headers: {
+            Authorization: `Bearer ${getAccessToken()}`,
+          },
+        })
+        .then((res) => {
+          setSeller(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [props.newedit]);
+
   return (
     <>
       <div>판매뚝딱 회원가입폼</div>
       <div className="registerForm">
         <InputGroup className="mb-3">
-          <FloatingLabel controlId="id" label="Id">
-            <Form.Control type="text" placeholder="id" />
+          <FloatingLabel controlId="id" label="ID(아이디)">
+            <Form.Control
+              type="text"
+              placeholder="id"
+              defaultValue={seller.sellerId || ""}
+            />
           </FloatingLabel>
           <Button variant="outline-secondary" id="button-addon2">
             Button
           </Button>
         </InputGroup>
-        <FloatingLabel controlId="password" label="Password" className="mb-3">
-          <Form.Control type="password" placeholder="Password" />
-        </FloatingLabel>
         <FloatingLabel
-          controlId="checkPW"
-          label="Check Password"
+          controlId="password"
+          label="Password(비밀번호)"
           className="mb-3"
         >
           <Form.Control type="password" placeholder="Password" />
         </FloatingLabel>
-        <FloatingLabel controlId="email" label="Email address" className="mb-3">
-          <Form.Control type="email" placeholder="name@example.com" />
+        <FloatingLabel
+          controlId="checkPW"
+          label="Check Password(비밀번호 확인)"
+          className="mb-3"
+        >
+          <Form.Control type="password" placeholder="Password" />
         </FloatingLabel>
-        <FloatingLabel controlId="name" label="Name" className="mb-3">
-          <Form.Control type="text" placeholder="name" />
+        <FloatingLabel
+          controlId="email"
+          label="Email address(이메일)"
+          className="mb-3"
+        >
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            defaultValue={seller.email || ""}
+          />
+        </FloatingLabel>
+        <FloatingLabel controlId="name" label="Name(성명)" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="name"
+            defaultValue={seller.name || ""}
+          />
         </FloatingLabel>
       </div>
     </>
--- src/Pages/UserFormPage.jsx
+++ src/Pages/UserFormPage.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
 import Register from "../Components/Register";
 import { useEffect } from "react";
 
 const UserFormPage = (props) => {
+  const newedit = useParams().newedit;
+  console.log(newedit);
+
   useEffect(() => {
     props.isBuyPageFunc(false);
   }, [props]);
@@ -9,7 +13,7 @@ const UserFormPage = (props) => {
   return (
     <div className="Pages">
       <div>판매뚝딱 판매자 회원가입/정보수정 폼 페이지입니다.</div>
-      <Register />
+      <Register newedit={newedit} />
     </div>
   );
 };
