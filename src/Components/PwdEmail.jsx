82045f549075d5dc25bd1d2d5928c3f15fcf81cc
codinglcy <codinglcy@gmail.com>
2023-04-18 18:19:47 +0900

    ✨ 임시비밀번호 발급 모달창 컴포넌트 파일 생성

---
+++ src/Components/PwdEmail.jsx
@@ -0,0 +1,13 @@
+import "./css/PwdEmail.css";
+import React from "react";
+// import Button from "react-bootstrap/Button";
+// import Modal from "react-bootstrap/Modal";
+// import Form from "react-bootstrap/Form";
+// import axiosApi from "../Util/api";
+// import { useNavigate } from "react-router-dom";
+
+const PwdEmail = () => {
+  return <></>;
+};
+
+export default PwdEmail;
