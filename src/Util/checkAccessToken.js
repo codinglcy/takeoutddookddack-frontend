366bc728bcd4e6859a8859e948568e20f428b627
codinglcy <codinglcy@gmail.com>
2023-04-20 17:15:08 +0900

    🔥 try catch 삭제

--- src/Util/checkAccessToken.js
+++ src/Util/checkAccessToken.js
@@ -10,39 +10,36 @@ const getAccessToken = () => {
     window.location.href = "/";
     return null;
   }
-  try {
-    axiosApi
-      .get("/api/seller/checkAccessToken", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        return accessToken;
-      })
-      .catch((err) => {
-        console.log("err", err);
-        axiosApi
-          .get(`/api/seller/checkRefreshToken?token=${refreshToken}`)
-          .then((res) => {
-            console.log("refresh", res);
-            accessToken = res.data.accessToken;
-            refreshToken = res.data.refreshToken;
-            localStorage.setItem("accessToken", accessToken);
-            localStorage.setItem("refreshToken", refreshToken);
-          })
-          .catch((err) => {
-            console.log("err", err);
-            alert("로그인 해주세요.");
-            localStorage.clear();
-            window.location.href = "/";
-          });
-      });
-    return accessToken;
-  } catch (err) {
-    console.log(err);
-  }
+
+  axiosApi
+    .get("/api/seller/checkAccessToken", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((res) => {
+      console.log(res.data, accessToken);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      axiosApi
+        .get(`/api/seller/checkRefreshToken?token=${refreshToken}`)
+        .then((res) => {
+          console.log("refresh", res);
+          accessToken = res.data.accessToken;
+          refreshToken = res.data.refreshToken;
+          localStorage.setItem("accessToken", accessToken);
+          localStorage.setItem("refreshToken", refreshToken);
+        })
+        .catch((err) => {
+          console.log("err", err);
+          alert("로그인 해주세요.");
+          localStorage.clear();
+          window.location.href = "/";
+        });
+    });
+
+  return accessToken;
 };
 
 export default getAccessToken;
